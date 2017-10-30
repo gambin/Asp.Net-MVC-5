446c6102ea5296b3189c6cd7585569794912e37b
Tarcisio Gambin <gambin.br@gmail.com>
2017-10-30 21:01:31 -0200

    Primeiro release. Exercicios para utilização de desenvolvimento em camadas e utilização de attribute routing

---
+++ Vidly/Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModel;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        [Route ("customers")]
+        public ActionResult Index()
+        {
+            // Old fashioned way
+            /*
+            List<Customer> cList = new List<Customer>();
+            Customer a = new Customer();
+            a.Id = 1;
+            a.Name = "John Smith";
+            Customer b = new Customer();
+            b.Id = 2;
+            b.Name = "Mary Willians";
+
+            cList.Add(a);
+            cList.Add(b);
+            */
+
+            // New way :)
+            List<Customer> cList = new List<Customer>()
+            {
+                new Customer {Id = 1, Name = "John Smith"},
+                new Customer {Id = 2, Name = "Maty Willians"}
+            };
+
+            CustomersViewModel viewModel = new CustomersViewModel
+            {
+                Customers = cList
+            };
+
+            return View(viewModel);
+        }
+
+        [Route("customers/{id}")]
+        public ActionResult Details(int id)
+        {
+            List<Customer> cList = new List<Customer>()
+            {
+                new Customer {Id = 1, Name = "John Smith"},
+                new Customer {Id = 2, Name = "Maty Willians"}
+            };
+
+            int posCliente = cList.FindIndex(x => x.Id == id);
+            Customer customer = cList[posCliente];
+
+            // return View(id);
+            return View("Details", customer);
+        }
+
+    }
+}
---
+++ Vidly/Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModel;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            List<Movie> mList = new List<Movie>()
+            {
+                new Movie {Id = 1, Name = "Shrek"},
+                new Movie {Id = 2, Name = "Wall-e"}
+            };
+
+            MoviesViewModel viewModel = new MoviesViewModel
+            {
+                Movies = mList
+            };
+
+            return View(viewModel);
+
+        }
+
+        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1,12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+    }
+}
