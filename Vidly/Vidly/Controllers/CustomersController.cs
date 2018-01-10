8fdd5b810cbc36b59612e8bbc6317e2430c86b39
Tarcisio Gambin <gambin.br@gmail.com>
2018-01-09 22:33:31 -0200

    Inclusao de Forms de Edicao, atualizacao de model e migration

--- Vidly/Vidly/Controllers/CustomersController.cs
+++ Vidly/Vidly/Controllers/CustomersController.cs
@@ -3,7 +3,8 @@ using System.Linq;
 using System.Web.Mvc;
 using System.Web.UI.WebControls;
 using Vidly.Models;
-using System.Data.Entity; 
+using System.Data.Entity;
+using Vidly.ViewModel;
 
 namespace Vidly.Controllers
 {
@@ -21,26 +22,74 @@ namespace Vidly.Controllers
             _context.Dispose();
         }
 
-        // GET: Customers
-        [Route ("customers")]
+        // GET: All Customers
+        [Route("customers")]
         public ActionResult Index()
         {
             var customers = _context.Customers.Include(c => c.MembershipType).ToList();
             return View(customers);
         }
 
+        //GET: Single Customer
         [Route("customers/{id}")]
         public ActionResult Details(int id)
         {
-            //List<Customer> cList = new List<Customer>()
-            //{
-            //    new Customer {Id = 1, Name = "John Smith"},
-            //    new Customer {Id = 2, Name = "Maty Willians"}
-            //};
-            // return View(id);
-
             var customer = _context.Customers.Include(c => c.MembershipType).SingleOrDefault(c => c.Id == id);
             return View(customer);
+        }
+
+        // NEW: Customer
+        [Route("customers/new")]
+        public ActionResult New()
+        {
+            var membershipTypes = _context.MembershipTypes.ToList();
+            var viewModel = new CustomerFormViewModel
+            {
+                MembershipTypes = membershipTypes
+                // ,Customer = _context.Customers.SingleOrDefault()
+
+            };
+            return View("CustomerForm", viewModel);
+        }
+
+        // EDIT: Customer
+        public ActionResult Edit(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                var viewModel = new CustomerFormViewModel
+                {
+                    Customer = customer,
+                    MembershipTypes = _context.MembershipTypes.ToList()
+                };
+                return View("CustomerForm", viewModel);
+            }
+        }
+
+        // SAVE: Customer
+        [HttpPost]
+        [Route("customers/save")]
+        public ActionResult Save(Customer customer)
+        {
+            if(customer.Id == 0)
+            {
+                _context.Customers.Add(customer);
+            } else
+            {
+                var customerInDb = _context.Customers.Single(c => c.Id == customer.Id);
+                customerInDb.Name = customer.Name;
+                customerInDb.Birthday = customer.Birthday;
+                customerInDb.IsSubscribedToNewsLetter = customer.IsSubscribedToNewsLetter;
+                customerInDb.MembershipTypeId = customer.MembershipTypeId;
+            }
+            
+            _context.SaveChanges();
+            return RedirectToAction("Index", "Customers");
         }
     }
 }
